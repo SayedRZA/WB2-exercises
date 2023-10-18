d56612e66a86f7f83987b26a940d8c6b4685617c
SayedRZA <reza.hoss777@gmail.com>
2023-10-18 11:46:16 -0700

    Fahrenheit to Celsius and the other way around

---
+++ MathScripts/fahrenheit2celsius.js
@@ -0,0 +1,19 @@
+// fahrenheit to celsius
+var fahrenheit = 68; 
+
+
+var celsius = (fahrenheit - 32) * (5/9);
+
+
+console.log("The temperature in Celsius is: " + celsius.toFixed(2) + " °C");
+
+
+
+// celsius to fahrenheit
+var celsius = 20; 
+
+
+var fahrenheit = (celsius * 9/5) + 32;
+
+
+console.log("The temperature in Fahrenheit is: " + fahrenheit.toFixed(2) + " °F");
--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -1,14 +1,6 @@
 
-var billAmount = parseFloat(prompt("20:"));
+var billAmount = parseFloat(prompt("$S20:"));
 var tipPercentage = parseFloat(prompt("15%:"));
 
 
-if (isNaN(billAmount) || isNaN(tipPercentage)) {
-  alert("Please enter valid numbers for the bill amount and tip percentage.");
-} else {
-  
-  var tipAmount = (billAmount * (tipPercentage / 100));
 
- 
-  alert("Tip amount: $" + tipAmount.toFixed(2));
-}
