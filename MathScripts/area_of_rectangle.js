0923d95b8efedd3b861660899cfa506d09ba6691
SayedRZA <reza.hoss777@gmail.com>
2023-10-18 10:35:39 -0700

    added the triangle area

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,10 @@
+function calculateTriangleArea(base, height) {
+    return (1/2) * base * height;
+  }
+  
+  // Example usage:
+  const base = 9;
+  const height = 6;
+  const area = calculateTriangleArea(base, height);
+  
+  console.log(`The area of the triangle is: ${area}`);
