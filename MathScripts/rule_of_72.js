01481cc8173143d16ccbe83df890aa71feb34d30
SayedRZA <reza.hoss777@gmail.com>
2023-10-18 11:32:12 -0700

    rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,8 @@
+
+
+var interestRate = 0.05; 
+
+var yearsToDouble = 72 / (interestRate * 100);
+
+
+console.log("It will take approximately " + yearsToDouble + " years for the savings account to double.");
