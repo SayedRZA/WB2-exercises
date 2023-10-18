9d93bc7dd377a8179ed94077844232995b9c65b3
SayedRZA <reza.hoss777@gmail.com>
2023-10-18 14:10:14 -0700

    distance in between

---
+++ MathScripts/distance.js
@@ -0,0 +1,10 @@
+
+var x1 = 2; 
+var y1 = 3; 
+var x2 = 5; 
+var y2 = 7; 
+
+var distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+
+console.log("The distance between the two points is: " + distance.toFixed(2));
